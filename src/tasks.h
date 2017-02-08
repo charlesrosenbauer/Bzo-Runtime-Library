93fedffdfc914166ecd50d5f955e4360ab3a0395
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-02-08 23:47:10 +0000

    Added some code for Tasks

---
+++ src/tasks.h
@@ -0,0 +1,86 @@
+#ifndef _TASK_HEADER
+#define _TASK_HEADER
+
+
+#include "stdint.h"
+
+
+
+
+
+
+
+
+
+
+//Builtin Support for Higher-Order Functions
+//This reduces complexity of writing parallel code.
+enum BzoHOFn{
+  BzHO_MAP,
+  BzHO_REDUCE,
+  BzHO_ESCAN,
+  BzHO_ISCAN,
+  BzHO_FILTER
+};
+
+
+
+
+
+
+
+
+
+
+typedef struct{
+  void(*fnptr)(void*);  //Function Pointer
+  void* param;          //Main Parameter(s)
+
+  /*
+    Higher-Order Function Specifications
+
+    hoType    : Type of Higher-Order Function
+    itemNum   : Number of items to run the task across. If == 1, the task is run as a normal function.
+    parStride : If the task is run as a Higher-Order function, param is treated as a pointer to an
+                  array of inputs. parStride is the size of each element in the array.
+  */
+  BzoHOFn hoType;
+  int itemNum;
+  int parStride;
+
+  //NUMA-centered
+  uint32_t threadReq;   //NUMA specific
+  uint64_t pars[4];     //Still supported for SMP, but not that useful for SMP.
+}BzoTask;
+
+
+
+
+
+
+
+
+
+
+typedef struct{
+  int top, bottom;
+  BzoTask tasks [8];
+}TaskQueue;
+
+
+
+
+
+
+
+
+
+
+typedef struct{
+
+}BzoTaskUnit;
+
+
+
+
+#endif
