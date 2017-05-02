da113a5be57774bb6c2defd1b1a7c6f82caa63d0
Charles Rosenbauer <charles.rosenbauer@gmail.com>
2017-05-02 23:48:18 +0000

    Added another stage to bzoInit()

--- src/bzo.c
+++ src/bzo.c
@@ -52,10 +52,40 @@ BzoStatus bzoInit(BzoEnvironment* env, int tnum){
   if(env  == NULL) return BZO_ERR_INV_PTR;
   if(tnum <  1   ) return BZO_ERR_INV_THD;
 
+  // Get dimensions of TaskUnit Grid
   int h, w;
   computeSize(tnum, &h, &w);
+  env->unitgrid = malloc(sizeof(BzoTaskUnit*) *  h * w);
 
-  env->unitgrid = malloc(sizeof(BzoTaskUnit) *  h * w);
+  // Initialize TaskUnit Grid
+  for(int it = 0; it < (h * w); it++){
+    if(it < tnum)
+      env->unitgrid[it] = malloc(sizeof(BzoTaskUnit));
+    else
+      env->unitgrid[it] = NULL;
+  }
+
+  // Allocate Map Queues
+  int stride = 1;
+  int toplevelnum;
+  do{
+    stride *= 4;
+    int cont = (stride < tnum);
+    if(cont){   // At top level, might be fewer than 4 nodes below
+      for(int it = 0; it < tnum; it += stride)
+        env->unitgrid[it].nmq+=toplevelnum;
+    }else{
+      toplevelnum = 0;
+      int maxIt = 0;
+      for(int it = 0; it < (tnum - stride); it += stride){
+        env->unitgrid[it].nmq+=4;
+        maxIt = it;
+      }
+      env->unitgrid[maxIt].nmq += (tnum % stride);
+    }
+  }while(stride < tnum);
+  for(int it = 0; it < tnum; it++)
+    env->unitgrid[it].mp = malloc(sizeof(TaskQueue) * (++env->unitgrid[it].nmq));
 
   return BZO_SUCCESS;
 }
